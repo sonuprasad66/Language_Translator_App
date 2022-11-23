1ace0cbd18e21a766cdc945828d2865eae8f5c63
sonuprasad66 <sp.srp1999@gmail.com>
2022-11-23 15:28:58 +0530

    Language_Translator_App

---
+++ language_translator_app/src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./Components/Navbar";
+import Translate from "./Components/Translate";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Translate />
+    </div>
+  );
+}
+
+export default App;
